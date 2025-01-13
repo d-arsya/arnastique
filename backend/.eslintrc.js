0e45b3070c0fbf01768eb4036b64499d9eff634f
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-01-13 19:54:00 +0700

    [STYLE] Change config file for eslint and prettier

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,17 +3,14 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended'],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
@@ -25,6 +22,6 @@ module.exports = {
     '@typescript-eslint/no-misused-promises': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-wrapper-object-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+}
