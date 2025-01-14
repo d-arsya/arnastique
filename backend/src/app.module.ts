58285e7b9eb8729d1ccd95edc9f544e88daae7c6
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-01-14 22:21:13 +0700

    [FIX] Change relative path

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { UserModule } from './user/user.module'
 import { WalletModule } from './wallet/wallet.module'
 import { CategoryModule } from './category/category.module'
 import { TransactionModule } from './transaction/transaction.module'
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [UserModule, WalletModule, CategoryModule, TransactionModule, AuthModule],
