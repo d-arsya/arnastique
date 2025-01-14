a0d129ca1c673403f6e4eea7722f638a1da9bf61
Kamaluddin Arsyad Fadllillah <102853169+d-arsya@users.noreply.github.com>
2025-01-15 02:21:05 +0700

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -38,11 +38,30 @@ import { AppModule } from './app.module';
 import express from 'express';
 import { createServer, proxy } from 'aws-serverless-express';
 import { Handler, Context, Callback } from 'aws-lambda';
-
+import { RequestMethod, ValidationPipe } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 const server = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.useGlobalPipes(new ValidationPipe())
+  const config = new DocumentBuilder()
+    .setTitle('Arnastique Backend API')
+    .setDescription('Arnastique Backend API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    // .addTag('Arnastique')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api/docs', app, documentFactory, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 }
+  })
+  app.setGlobalPrefix('api', {
+    exclude: [
+      { path: '/', method: RequestMethod.GET },
+      { path: '/api', method: RequestMethod.GET }
+    ]
+  })
   await app.init();
 }
 
